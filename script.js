820337bdd5cb1284a7270c1854da121469c64b07
Kiendz <kiennguyen3112@gmail.com>
2025-11-26 11:19:31 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+//in-out sidebar
 const sideBar = document.querySelector(".sidebar-content");
 const toggleBtn = document.querySelector(".sidebar-toggler");
 
 toggleBtn.addEventListener("click", () => {
   sideBar.classList.toggle("closed");
 });
+
+//login button
+const loginBtn = document.querySelector(".button-gr-login");
+const newDishBtn = document.querySelector(".new-dish")
+
+const loginTransaction = (button)=>{
+  button.addEventListener("click", ()=>{
+    window.location.href = "loginSite.html";
+  });
+}
+loginTransaction(loginBtn);
+loginTransaction(newDishBtn);
