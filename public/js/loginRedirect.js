eee6257e672898d9ff6ee2a32a54673c158d1079
zuy3ka <email@example.com>
2025-12-08 15:21:54 +0700

    Finding-filter

--- public/js/loginRedirect.js
+++ public/js/loginRedirect.js
@@ -3,8 +3,8 @@ const loginBtn = document.querySelector(".button-gr-login");
 const newDishBtn = document.querySelector(".new-dish");
 const findDish = document.querySelector(".findingbox-btn");
 
-const loginTransaction = (button)=>{
-  button.addEventListener("click", ()=>{
+const loginTransaction = (button) => {
+  button.addEventListener("click", () => {
     window.location.href = "loginSite.html";
   });
 }
--- public/js/showInfo.js
+++ public/js/showInfo.js
@@ -1,12 +1,12 @@
 function show() {
-var menu = document.getElementById("dropdown-menu");
-menu.classList.toggle("show");
+    var menu = document.getElementById("dropdown-menu");
+    menu.classList.toggle("show");
 }
 window.onclick = function (event) {
     if (!event.target.matches(".user-box-img")) {
         var menu = document.getElementById("dropdown-menu");
         if (menu && menu.classList.contains("show")) {
-        menu.classList.remove("show");
+            menu.classList.remove("show");
         }
     }
 };
